4803ed291a871a1c01b426c2ccb5882ac56404a1
Amos Coats <coats01@hotmail.com>
2018-04-10 15:43:50 +0200

    Initial commit

---
+++ persistance-service/src/main/java/com/amos/lukkien/airlineapp/persistanceservice/PersistanceServiceApplication.java
@@ -0,0 +1,12 @@
+package com.amos.lukkien.airlineapp.persistanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistanceServiceApplication.class, args);
+	}
+}
---
+++ persistance-service/src/test/java/com/amos/lukkien/airlineapp/persistanceservice/PersistanceServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.amos.lukkien.airlineapp.persistanceservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersistanceServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
